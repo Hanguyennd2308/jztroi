c677d41f26ceb67bd6fe6e29e32e82286b683390
NguyenHa2308 <dieuhanguyenngoc@gmail.com>
2024-03-30 17:57:15 +0700

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,15 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        booking: './src/booking.js',
+        treatment: './src/treatment.js',
+        bill: './src/bill.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].bundle.js'
     },
     watch: true
 }
